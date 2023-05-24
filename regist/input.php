517997df5e637dd48eb4c0a3670690abc5d758f6
{marimo_7} <{220098tt@yse-c.net}>
2023-05-24 11:03:03 +0900

    first commit

---
+++ regist/input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regist</title>
+</head>
+<body>
+    <h1>入力画面</h1>
+    <form action="" method="post"></form>
+    <div>
+        <label for="">氏名</label>
+        <input type="passwoerd" name="password">
+    </div>
+    <button>確認</button>
+</body>
+</html>
